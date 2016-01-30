c58fe966d6b9dd202bb8ac784f66833aa2a6ec43
Ian Shusdock <ishusdock@gmail.com>
2016-01-30 13:22:57 -0500

    Added forward pass for neural network. Also corrected variable assignments so types are correct

--- ann.py
+++ ann.py
@@ -1,5 +1,5 @@
 import argparse
-import numpy
+import numpy as num
 
 "Parse through the list of arguements and return the value of the passed argument"
 "list_of_args is the list of arguments in the format [arg1, val1, arg2, val2, etc]"
@@ -14,10 +14,37 @@ def get_argument(list_of_args, arg):
         counter = counter + 1
     return 0
 
-def generate_neural_network(list_of_points, hidden_nodes):
+"Generate the hidden layer of the neural network with random weights on each node"
+"hidden_nodes is the number of hidden nodes to use"
+def generate_neural_network(hidden_nodes):
 	hidden_layer = []
 	for x in range(0, hidden_nodes):
-		return
+		hidden_layer.append({"weightX": num.random.random(), "weightY": num.random.random()})
+	return hidden_layer
+
+"Run the forward pass of the neural network for a given point"
+"hidden_layer is the layer of hidden nodes to use"
+"output_node is the output node to us"
+"point is the current point to use"
+def run_forward_neural_network(hidden_layer, output_node, point):
+	
+	out_exp = 0
+		
+	"For each hidden node, send the output value to the output node"
+	for node in hidden_layer:
+		"Compute the exponent in the sigmoid function for the node"
+		node_exp = node["weightX"] * point["x_val"] + node["weightY"] * point["y_val"]
+		"Compute the value of the sigmoid function for the node"
+		node_val = 1/(1+num.exp(-node_exp))
+		"Add the sigmoid value of the node to the exponent of the output node"
+		out_exp = out_exp + node_val
+		
+	"Compute the sigmoid value for the output node"	
+	out_val = 1/(1+num.exp(-out_exp))
+	"Round to the nearest number"
+	out_val = num.round(out_val)
+
+	return out_val
 		
 
 parser = argparse.ArgumentParser()
@@ -27,8 +54,8 @@ args = parser.parse_args()
 
 "Parse the arguments"
 file_name = args.file_name
-hidden_nodes = get_argument(args.optional_arguments, 'h')
-holdout_percent = get_argument(args.optional_arguments, 'p')
+hidden_nodes = int(get_argument(args.optional_arguments, 'h'))
+holdout_percent = int(get_argument(args.optional_arguments, 'p'))
 
 "Read the file and store it as a list of lines"
 with open(file_name) as f:
@@ -43,5 +70,8 @@ for line in file_content:
 		continue;
 
 	"The last character of the line will be \n so it must be removed"
-	points.append({"x_val": temp[0], "y_val": temp[1], "class": temp[2][:-1]})
- 
+	points.append({"x_val": float(temp[0]), "y_val": float(temp[1]), "category": float(temp[2][:-1])})
+
+hidden_layer = generate_neural_network(hidden_nodes)
+output_node = {"weightX": num.random.random(), "weightY": num.random.random()}
+
